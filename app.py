db410f67fa74edbb0cff4131f7b594c9fd649af5
Shakti Ranjan Mohanty <109822979+shakti468@users.noreply.github.com>
2025-05-29 01:34:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask CI/CD!"
+    return "Hello, Flask CI/CD is running successfully on EC2 with Jenkins and Nginx!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
